18005f570e830e841d37d0c1ca1b4cfac9c8fb64
ZachatorCodes <104332807+ZachatorCodes@users.noreply.github.com>
2024-02-18 04:43:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Basic Python Data Types
+
+# String
+print("Hello World!")
+print("Hello World!"[0])
+print("123" + "456")
+
+# Int
+print(5)
+print(123_456_789) # Interpreted as 123456789
+print(123 + 456)
+
+# Float
+print(3.14159)
+print(62.15 + 7)
+print(123.456)
+
+# Boolean
+# Note: in Python, boolean are capitalized
+print(True)
+print(False)
