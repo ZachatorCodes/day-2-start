54bac634308b347fe6e18662b92b01dbd8857205
ZachatorCodes <104332807+ZachatorCodes@users.noreply.github.com>
2024-02-19 19:34:51 +0000

    Learned about mathematical operations in Python

---
+++ math_operations.py
@@ -0,0 +1,8 @@
+# Addition 1 + 1
+# Subtraction 1 - 1
+# Multiplication 1 * 1
+# Division 1 / 1
+# Expontiation 1 ** 1
+
+# When dividing, you always end up with a floating point number.
+print(type(6 / 3))
