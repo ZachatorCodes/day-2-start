41ca8ab7cd29030359bf59a81c7d6e83179e15b8
ZachatorCodes <104332807+ZachatorCodes@users.noreply.github.com>
2024-02-19 20:51:32 +0000

    Learned about f-strings and number manipulation

---
+++ main.py
@@ -0,0 +1,16 @@
+# Round takes in a value and how many decimal points to round it to
+print(round((8 / 3), 2))
+
+# Floor division: chops off the decimal (doesn't round)
+print(8 // 3)
+
+score = 0
+score += 1  # Adds one to score
+
+height = 10.6
+weight = 3719.572
+trueOrFalse = True
+
+# F String - A way to interpolate strings with auto type casting
+
+print(f"Your score is {score}. Your height and weight are {height} / {weight} respectively. The fact that you are a human is {trueOrFalse}.")
